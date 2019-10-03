c08c5669b63a007ccbd77a39aa6a4e2582cb5dc9
Josué Gimenes <josuegalvares@gmail.com>
2019-10-03 12:33:11 -0300

    chore: Handling exception and error for duplicate entries and a few more things

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Form, SubmitButton, List } from './styles';
+
+import { Form, SubmitButton, List, ShowError, NoShowError } from './styles';
 
 export default class Main extends Component {
   state = {
     newRepo: '',
     repositories: [],
     loading: false,
-    error: null,
+    hasError: null,
+    hasErrorMess: '',
   };
 
   // Carregar os dados do localStorage
@@ -34,18 +36,23 @@ export default class Main extends Component {
   }
 
   handleInputChange = e => {
-    this.setState({ newRepo: e.target.value, error: null });
+    this.setState({ newRepo: e.target.value });
   };
 
   handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({ loading: true, error: false });
+    this.setState({ loading: true, hasError: false });
 
     try {
       const { newRepo, repositories } = this.state;
 
-      if (newRepo === '') throw 'Você precisa indicar um repositório';
+      if (newRepo === '')
+        throw new Error('Você precisa indicar um repositório');
+
+      const hasRepo = repositories.find(r => r.name === newRepo);
+
+      if (hasRepo) throw new Error('Repositório duplicado');
 
       const response = await api.get(`/repos/${newRepo}`);
 
@@ -56,16 +63,29 @@ export default class Main extends Component {
       this.setState({
         repositories: [...repositories, data],
         newRepo: '',
+        hasErrorMess: '',
       });
-    } catch (err) {
-      this.setState({ error: true });
+    } catch (Error) {
+      this.setState({
+        hasError: true,
+        hasErrorMess:
+          Error.message === 'Request failed with status code 404'
+            ? 'Repositório não encontrado'
+            : Error.message,
+      });
     } finally {
       this.setState({ loading: false });
     }
   };
 
   render() {
-    const { newRepo, repositories, loading, error } = this.state;
+    const {
+      newRepo,
+      repositories,
+      loading,
+      hasError,
+      hasErrorMess,
+    } = this.state;
 
     return (
       <Container>
@@ -74,15 +94,14 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form onSubmit={this.handleSubmit} error={error}>
+        <Form onSubmit={this.handleSubmit} error={hasError ? 1 : 0}>
           <input
             type="text"
             placeholder="Adicionar repositório"
             value={newRepo}
             onChange={this.handleInputChange}
           />
-
-          <SubmitButton loading={loading}>
+          <SubmitButton loading={loading ? 1 : 0}>
             {loading ? (
               <FaSpinner color="#fff" size={14} />
             ) : (
@@ -91,9 +110,21 @@ export default class Main extends Component {
           </SubmitButton>
         </Form>
 
+        <NoShowError hasError={hasError}>
+          <small>
+            Ex: <i>UserName/RepositoryName</i>
+          </small>
+        </NoShowError>
+
+        {hasErrorMess && (
+          <ShowError>
+            <small>{hasErrorMess}</small>
+          </ShowError>
+        )}
+
         <List>
           {repositories.map(repository => (
-            <li key={repositories.name}>
+            <li key={repository.name}>
               <span>{repository.name}</span>
               <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
                 Detalhes
